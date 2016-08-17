3f1509fc8f2ac9958410f3ca630a463fb3236f37
Eduardo Zamora <ezamora@cambridgeassociates.com>
2016-08-16 18:12:54 -0600

    New updates

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,14 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = webpackMerge(commonConfig, {
+    plugins: [
+      new LiveReloadPlugin({
+        port: 8080,
+        appendScriptTag: true
+      })
+    ],
     module: {
         preLoaders: [
             {
